d46a983c46068145bd5eb397169dc4c513ca1373
3D2YArley <arleybernal012@gmail.com>
2024-05-25 14:22:27 -0500

    Se agrega clase NodoGrafo para implementar un grafo de diccioanrio.

---
+++ src/NodoGrafo.cpp
@@ -0,0 +1,44 @@
+#include "NodoGrafo.h"
+
+NodoGrafo::NodoGrafo() {}
+
+NodoGrafo::NodoGrafo(const Palabra& palabra) : palabra(palabra) {}
+
+Palabra NodoGrafo::getPalabra() const {
+    return palabra;
+}
+
+void NodoGrafo::setPalabra(const Palabra& palabra) {
+    this->palabra = palabra;
+}
+
+int NodoGrafo::getNum_vecinos() const {
+    return num_vecinos;
+}
+
+void NodoGrafo::setNum_vecinos(const int vecinos) {
+    this->num_vecinos = vecinos;
+}
+
+list<NodoGrafo*>& NodoGrafo::getAdyacentes() {
+    return listaAdyacencia;
+}
+
+void NodoGrafo::addAdyacente(NodoGrafo* nodo) {
+    listaAdyacencia.push_back(nodo);
+}
+
+void NodoGrafo::deleteAdyacente(NodoGrafo* nodo) {
+    if (findAyacente(nodo)) {
+        listaAdyacencia.remove(nodo);
+    }
+}
+
+bool NodoGrafo::findAyacente(NodoGrafo* nodoBuscado) {
+    for (NodoGrafo* vecino : listaAdyacencia){
+        if (vecino->getPalabra().getWord() == nodoBuscado->getPalabra().getWord()){
+            return true;
+        }
+    }
+    return false;
+}
